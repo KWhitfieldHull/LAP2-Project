c5bfb2b9017749097fee603cbf9f003aa32e1c59
Adil Wadud <adilwadud10@gmail.com>
2023-10-11 16:31:38 +0100

    update details

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -36,6 +36,29 @@ async function login(req, res) {
     }
 }
 
+async function showUserByToken(req, res) {
+    try {
+        const data = req.body
+        const user = await Token.getUserByToken(data)
+        res.status(201).send({ user })
+      } catch (err) {
+        res.status(400).send({ "error": err.message })
+      }
+}
+
+
+async function update (req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        const data = req.body;
+        const user = await User.getOneById(id);
+        const result = await user.update(data);
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(404).json({error: err.message})
+    }
+}
+
 module.exports = {
-    register, login
+    register, login, showUserByToken, update
 }                           
