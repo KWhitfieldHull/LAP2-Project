3941094efe6d4efcdee46ceec28f83818230b0ea
IldikoMagda <ildiko.magda@gmail.com>
2023-10-12 13:11:31 +0100

    minor changes to variables in countdown function

--- client/assets/js/countdown.js
+++ client/assets/js/countdown.js
@@ -36,10 +36,10 @@ x();
     let distance = countDownDate - now;
   
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
     // Display the result in the element with id="demo"
     document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
